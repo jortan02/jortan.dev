b5416acf4b4fcbb3cbecdc1f76160be5f936e40e
Jordan Tan <jortandev+github@gmail.com>
2024-01-01 12:23:09 -0700

    Updated particle effects Merge branch 'master' of https://github.com/jortan02/jortan.dev

--- src/components/ParticlesContainer.tsx
+++ src/components/ParticlesContainer.tsx
@@ -48,39 +48,19 @@ export const ParticlesContainer = () => {
         },
         shape: {
           type: "circle",
-          stroke: {
-            width: 0,
-            color: "#000000"
-          },
-          polygon: {
-            nb_sides: 5
-          }
         },
         opacity: {
-          value: 0.5,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false
-          }
+          value: 1,
         },
         size: {
           value: 1,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40.00000000000001,
-            size_min: 0.1,
-            sync: false
-          }
         },
         links: {
           color: "#a1a1aa",
           distance: 200,
           enable: true,
-          width: 1
+          width: 1,
+          opacity: 1
         },
         move: {
           enable: true,
@@ -88,57 +68,18 @@ export const ParticlesContainer = () => {
           direction: "none",
           random: false,
           straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200
-          }
+          outModes: "bounce",
         }
       },
       interactivity: {
         detect_on: "canvas",
         events: {
-          onhover: {
-            enable: false,
-            mode: "repulse"
-          },
-          onclick: {
-            enable: false,
-            mode: "push"
-          },
           resize: {
             enable: true
           }
         },
-        modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 1
-            }
-          },
-          bubble: {
-            distance: 400,
-            size: 40.00000000000001,
-            duration: 2,
-            opacity: 8,
-            speed: 3
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4
-          },
-          push: {
-            particles_nb: 4
-          },
-          remove: {
-            particles_nb: 2
-          }
-        }
       },
-      retina_detect: true
+      detectRetina: true
     }),
     [],
   );

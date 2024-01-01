c6d982e619e4c3cad910708c5facc78314d4ffe2
Jordan Tan <jortandev+github@gmail.com>
2024-01-01 01:21:04 -0700

    Updated particle effects

--- src/components/ParticlesContainer.tsx
+++ src/components/ParticlesContainer.tsx
@@ -35,49 +35,110 @@ export const ParticlesContainer = () => {
 
   const options: ISourceOptions = useMemo(
     () => ({
-        background: {
-            color: {
-                value: "#171717",
-            },
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800
+          }
         },
-        particles: {
-            color: {
-                value: "#a1a1aa",
-            },
-            links: {
-                color: "#a1a1aa",
-                distance: 200,
-                enable: true,
-            },
-            move: {
-                enable: true,
-                outModes: {
-                    default: "out",
-                },
-                random: true,
-                speed: 0.2,
-                straight: true,
-            },
-            number: {
-                value: 80,
-                density: {
-                  enable: true,
-                  value_area: 800
-                }
-            },
-            size: {
-                value: 1,
-            },
-            interactivity: {
-              detect_on: "canvas",
-              events: {
-                resize: {
-                  enable: true
-                }
-              },
-            },
-            
+        color: {
+          value: "#a1a1aa"
         },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000"
+          },
+          polygon: {
+            nb_sides: 5
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 1,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40.00000000000001,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        links: {
+          color: "#a1a1aa",
+          distance: 200,
+          enable: true,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 0.2,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: false,
+            mode: "repulse"
+          },
+          onclick: {
+            enable: false,
+            mode: "push"
+          },
+          resize: {
+            enable: true
+          }
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40.00000000000001,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
     }),
     [],
   );

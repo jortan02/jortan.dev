c844694a2bd16f665c617d198f40978d30da3a11
Jordan Tan <jortandev+github@gmail.com>
2024-01-05 13:23:02 -0700

    Added SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.scss";
 
 const font = JetBrains_Mono({
@@ -25,6 +26,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${font.className}`}>{children}</body>
+            <SpeedInsights />
         </html>
     );
 }

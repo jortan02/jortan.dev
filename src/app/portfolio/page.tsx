6326bf3dae8116bf76ecfb22a5e0245a32645ad1
Jordan Tan <jortandev+github@gmail.com>
2024-01-05 00:32:34 -0700

    Sort fix for smaller screens

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,17 +5,22 @@ import { allPortfolios } from "contentlayer/generated";
 import Image from "next/image";
 
 export default async function PortfolioIndex() {
-    const sorted = allPortfolios.filter((item) => item.published).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    const sorted = allPortfolios
+        .filter((item) => item.published)
+        .sort(
+            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+        );
     return (
         <div className="min-h-screen flex flex-col justify-start">
             <Navigation />
             <main className="px-4 pb-16 mx-auto flex-grow max-w-4xl space-y-8">
-				<div>
-                	<h1 className="text-neutral-50 text-4xl">Portfolio</h1>
-					<div className="mt-4 w-full h-px bg-neutral-400" />
-				</div>
+                <div>
+                    <h1 className="text-neutral-50 text-4xl">Portfolio</h1>
+                    <div className="mt-4 w-full h-px bg-neutral-400" />
+                </div>
 
-                <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+                {/* Larger screens */}
+                <div className="hidden md:flex flex-row space-y-0 space-x-4">
                     <div className="flex w-full flex-col space-y-4">
                         {sorted
                             .filter((_, index) => index % 2 === 0)
@@ -35,6 +40,15 @@ export default async function PortfolioIndex() {
                                     item={project}
                                 />
                             ))}
+                    </div>
+                </div>
+
+                {/* Smaller screens */}
+                <div className="flex md:hidden flex-row space-y-4">
+                    <div className="flex w-full flex-col space-y-4">
+                        {sorted.map((project) => (
+                            <PortfolioCard key={project.slug} item={project} />
+                        ))}
                     </div>
                 </div>
             </main>
